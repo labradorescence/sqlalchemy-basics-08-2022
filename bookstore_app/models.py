e240c39b69ce27a863e51a606098019f2409f436
Ben Botsford <ben.botsford@flatironschool.com>
2022-08-18 21:59:47 -0500

    finish section

--- bookstore_app/models.py
+++ bookstore_app/models.py
@@ -13,6 +13,19 @@ class Book(Base):
     author = Column(String())
     publisher = Column(String())
     publish_date = Column(DateTime())
+    cost = Column(Integer())
 
     def __repr__(self):
         return f'Book({self.title} by {self.author})'
+
+class Salesperson(Base):
+    __tablename__ = 'salespeople'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    birthday = Column(DateTime())
+    last_clocked_in = Column(DateTime())
+    last_clocked_out = Column(DateTime())
+
+    def __repr__(self):
+        return f'Salesperson({self.name})'
